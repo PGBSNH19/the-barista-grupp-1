c5eb04f471fc824d95bc6de253ff9ed66ad6787b
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-03 14:03:51 +0100

    @robotdostopif

--- Barista/CoffeMachine.cs
+++ Barista/CoffeMachine.cs
@@ -7,12 +7,25 @@ namespace Barista
     class CoffeMachine
     {
         public Bean  bean;
-        public string WaterSource { get; set; }
+        public Water WaterSource { get; set; }
         public float Temperature { get; set; }
         public float Pressure { get; set; }
         public float Waterlevel { get; set; }
         public float BeansAmount { get; set; }
-        public float MilkAmpount { get; set; }
+        public float MilkAmount { get; set; }
         public float CupSize { get; set; }
+
+        public AddWater()
+        public void SetTemperature(float _temperature)
+        {
+            
+        }
+
+        public void GrindBean()
+        {
+
+        }
+
+        public void 
     }
 }
