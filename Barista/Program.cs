bb65bfc16f2677fe18da34c6b5dd54dd172bb177
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-03 15:42:34 +0100

    @robotdostopif

--- Barista/Program.cs
+++ Barista/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Barista
 {
@@ -6,6 +7,35 @@ namespace Barista
     {
         static void Main(string[] args)
         {
+            List<IIngredient> Ingredients = new List<IIngredient>();
+            Ingredients.Add(new Bean { 
+                Name = "Mellan Rost", 
+                Type = "Robusta", 
+                Manufacturer = "Gevalia", 
+                Price = 45, Unit = "g", 
+                Ecological = false, 
+                Fairtrade = false, 
+                RoastType = "Mellan Rost" });
+            Ingredients.Add(new Bean { 
+                Name = "Mellan Rost", 
+                Type = "Arabica", 
+                Manufacturer = "Löfbergs", 
+                Price = 55, 
+                Unit = "g", 
+                Ecological = true, 
+                Fairtrade = true, 
+                RoastType = "Mellan Rost" });
+
+            Ingredients.Add(new Water { 
+                Name = "Loka Citron", 
+                Type = "Still", 
+                Manufacturer = "Loka", 
+                Price = 13, 
+                Unit = "l" });
+
+            Ingredients.Add(new Milk { Name="iKaffe", Type="Havre",  });
+
+
             Start();
 
         }
