63b7bf1a27e04cc9aef9b198efc3ffb20498b318
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-03 13:42:32 +0100

    @MirkoPralica

---
+++ Barista/CoffeMachine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barista
+{
+    class CoffeMachine
+    {
+        public Bean  bean;
+        public string WaterSource { get; set; }
+        public float Temperature { get; set; }
+        public float Pressure { get; set; }
+        public float Waterlevel { get; set; }
+        public float BeansAmount { get; set; }
+        public float MilkAmpount { get; set; }
+        public float CupSize { get; set; }
+    }
+}
---
+++ Barista/Cup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Barista
+{
+    class Cup
+    {
+        public int Size { get; set; }
+        public string Material { get; set; }
+    }
+}
