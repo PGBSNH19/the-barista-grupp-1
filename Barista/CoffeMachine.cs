f80393bf64a193424381ed1856216a12b09eff21
Hampus Kjellstrand <hampe.kjellstrand@gmail.com>
2020-03-05 16:32:42 +0100

    @Gerglamesh

--- Barista/CoffeMachine.cs
+++ Barista/CoffeMachine.cs
@@ -1,37 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
-namespace Barista
-{
-    public class CoffeeMachine
+namespace Barista {
+    public class CoffeeMachine : IStart, IEspresso, IStop
     {
+        private List<Ingredient> ingredients;
+        public Espresso espresso { get; set; }
 
-        private CoffeeMachine() { }
-
-        //public IStart Start() => new CoffeeMachine();
-
-        public void AddEspresso(int amount)
+        public IEspresso Start() 
         {
+            this.ingredients = new List<Ingredient>();
+            return this;
+        }
 
+        public IStop AddEspresso(int amount)
+        {
+            this.espresso = (new Espresso(
+                (Bean) Program.Ingredients.Where(i => i.Name == "Mellan Rost").First(),
+                (Water)Program.Ingredients.Where(i => i.Name == "Loka Citron").First()));
+            return this;
+        }
+
+        public Coffee GetCoffee()
+        {
+            return new Coffee(this.ingredients);
         }
 
     }
 
     public interface IStart
     {
-        public void AddEspresso(int Amount);
+        public IEspresso Start();
+    }
+
+    public interface IEspresso
+    {
+        public IStop AddEspresso(int Amount);
     }
 
     public interface IStop
     {
-
+        public Coffee GetCoffee();
     }
 
-    public class Espresso
+
+
+    public class Coffee
     {
-        Bean bean;
-        Water water;
+        public List<Ingredient> Ingredients { get; set; }
+        public Coffee(List<Ingredient> ingredients)
+        {
+            this.Ingredients = ingredients;
+        }
+    }
+
+
+    public class Espresso : Ingredient
+    {
+        public Bean bean { get; }
+        public Water water { get; }
 
         public int Amount { get; set; }
 
--- Barista/Program.cs
+++ Barista/Program.cs
@@ -8,15 +8,88 @@ namespace Barista
     {
         static void Main(string[] args)
         {
-            
 
-            Start();
-
+            CoffeeMachine cm = new CoffeeMachine();
+            Coffee c = cm.Start().AddEspresso(1).GetCoffee();
+            //c.Ingredients[0].Name = "Bönan";
+            Console.WriteLine(((Espresso)c.Ingredients[0]).bean.);
         }
         public static void Start()
         {
            
         }
+
+
+        
+        public static List<Ingredient> Ingredients = new List<Ingredient>()
+        {
+            new Bean {
+                Name = "Mellan Rost",
+                Type = "Robusta",
+                Manufacturer = "Gevalia",
+                Price = 45, Unit = "g",
+                Ecological = false,
+                Fairtrade = false,
+                RoastType = "Mellan Rost" },
+
+            new Bean {
+                Name = "Mellan Rost",
+                Type = "Arabica",
+                Manufacturer = "Löfbergs",
+                Price = 55,
+                Unit = "g",
+                Ecological = true,
+                Fairtrade = true,
+                RoastType = "Mellan Rost" },
+
+            new Water {
+                Name = "Loka Citron",
+                Type = "Still",
+                Manufacturer = "Loka",
+                Price = 13,
+                Unit = "l" },
+
+            new Milk {
+                Name = "iKaffe",
+                Type = "Havre",
+                Ecological = true,
+                Fat = 0.5f,
+                Lactose = false,
+                Manufacturer = "Oatly",
+                Price = 20,
+                Unit = "l"
+            },
+
+            new Milk
+            {
+                Name = "Mellanmjölk",
+                Type = "Ko-mjölk",
+                Ecological = false,
+                Fat = 1.5f,
+                Lactose = true,
+                Manufacturer = "Arla",
+                Price = 14,
+                Unit = "l"
+            },
+
+            new Flavourings
+            {
+                Name = "Socker",
+                Type = "Strö",
+                Manufacturer = "Dansukker",
+                Price = 17,
+                Unit ="kg"
+            },
+
+            new Flavourings
+            {
+                Name = "Kardemumma",
+                Type = "Krydda",
+                Manufacturer = "Santa Maria",
+                Price = 1,
+                Unit ="g"
+            }
+        };
     }
 }
 
